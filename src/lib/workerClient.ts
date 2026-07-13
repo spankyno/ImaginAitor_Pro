94b3f5a8aaca2f3e71d2fd7194e77d83812f654d
Kalbo <spankyelmalo@yahoo.es>
2026-07-13 08:44:14 +0200

    Delete src directory

--- src/lib/workerClient.ts
+++
@@ -1,6 +0,0 @@
-import * as Comlink from 'comlink'
-import type { RenderWorkerApi } from '../workers/render.worker'
-
-// Single shared worker instance for the whole app.
-const worker = new Worker(new URL('../workers/render.worker.ts', import.meta.url), { type: 'module' })
-export const renderWorker = Comlink.wrap<RenderWorkerApi>(worker)
